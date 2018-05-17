d19f3f890882139311c3fc10a616f2c008ecd673
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-17 23:29:57 +0530

    insert.js

---
+++ insert/insert.js
@@ -0,0 +1,29 @@
+//import express module
+var express = require("express");
+
+//import connection 
+var conn = require("connection");
+
+//getting the connection object
+var connection =conn.getConnection();
+
+//connect to database
+connection.connect();
+
+//create the module(Router)
+var router = express.Router;
+router.post("/",function(req,res){
+    //reading the parameters from client
+    var p_id=req.body.p_id;
+    var p_name=req.body.p_name;
+    var p_cost=req.body.p_cost;
+
+    connection.query("insert the product values ("+p_id+",'"+p_name+"', "+p_cost+")"
+        function (err,result){
+        if(err){
+            console.log("error while inserting the object!")
+        }else{
+            res.send({insert:"success"});
+        }
+    });
+});
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express= require("express");
 var fetch=require("./fetch/fetch");
+var bodyparser= require("body-parser");
 
 var app=express();
 app.use("/fetch"/fetch);
+
+app.use(bodyparser.JSON()); //set JSON as MIME type
+app.use(bodyparser.urlencoded({extended:false})); //front-end not sending any form data
+var insert = require("./insert/insert");
+app.use("/insert,insert");
 app.listen(8080);
 console.log("server is listening at port no. 8080");
