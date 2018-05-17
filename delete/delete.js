a510e74533c3c4e215fc2359e9af0828a0575022
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-17 23:46:37 +0530

    delete.js

---
+++ delete/delete.js
@@ -0,0 +1,29 @@
+//import express module
+var express = require("express");
+
+//import connection 
+var conn = require("../config/db_connection");
+
+//getting the connection object
+var connection =conn.getConnection();
+
+//connect to database
+connection.connect();
+
+//create the module(Router)
+var router = express.Router;
+router.post("/",function(req,res){
+    //reading the parameters from client
+    var p_id=req.body.p_id;
+    
+
+    connection.query("delete from product where p_id="p_id,
+    function(err){
+        if (err){
+            console.log("error while deleting the data !");
+        }else{
+            res.send({delete:"success"});
+        }
+    });
+});
+module.exports=router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express= require("express");
 var fetch=require("./fetch/fetch");
 var bodyparser= require("body-parser");
+var update= require("./update/update");
+var remove=require("./delete/delete");
 
 var app=express();
 app.use("/fetch"/fetch);
@@ -9,5 +11,7 @@ app.use(bodyparser.JSON()); //set JSON as MIME type
 app.use(bodyparser.urlencoded({extended:false})); //front-end not sending any form data
 var insert = require("./insert/insert");
 app.use("/insert,insert");
+app.use("update",update);
+app.use("delete",remove);
 app.listen(8080);
 console.log("server is listening at port no. 8080");
