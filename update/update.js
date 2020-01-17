be98ba45b5f2d7a137d9fcc0656a3eb91d42af56
Pankaj Singh <singh.pankaj@live.com>
2020-01-17 13:41:24 +0530

    Update update.js

--- update/update.js
+++ update/update.js
@@ -1,31 +1,31 @@
-//import express module
-var express = require("express");
+// import module
+const express = require("express");
 
-//import connection 
-var conn = require("../config/db_connection");
+// import connection 
+let conn = require("../config/db_connection");
 
-//getting the connection object
+// getting the connection object
 var connection = conn.getConnection();
 
-//connect to database
+// connect to database
 connection.connect();
 
-//create the module(Router)
-var router = express.Router();
-router.post("/",function(req,res){
-//reading the parameters from client
+// create the module(Router)
+let router = express.Router();
+router.post("/",function(req, res) {
+// reading the parameters from client
     var p_id=req.body.p_id;
     var p_name=req.body.p_name;
     var p_cost=req.body.p_cost;
 
     connection.query("update product set p_name ='"+p_name +"',p_cost="+p_cost+" where p_id = "+ p_id,
-    function(err,result){
-        if (err){
+    function(err,result) {
+        if (err) {
             console.log("error while updating the data !");
-        }else{
-            res.send({update:"success"});
+        } else {
+            res.send({ update:"success" });
         }
     });
 });
-//export router
-module.exports=router;
+
+module.exports = router;
