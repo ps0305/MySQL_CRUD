eba3aaea50bd2454398478e5dbb1e943b5c93310
Pankaj Singh <singh.pankaj@live.com>
2020-01-17 13:42:39 +0530

    Update db_properties.js

--- config/db_properties.js
+++ config/db_properties.js
@@ -1,6 +1,6 @@
-module.exports={
-    host:"localhost",
-    user:"root",
-    password:"root",
-    database:"nodedb"
+module.exports = {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "nodedb"
 };
