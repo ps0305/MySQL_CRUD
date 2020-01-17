87a95426fc00253c0d3f33eee95c390371290147
Pankaj Singh <singh.pankaj@live.com>
2020-01-17 13:45:45 +0530

    Update insert.js

--- insert/insert.js
+++ insert/insert.js
@@ -1,31 +1,30 @@
-//import express module
-var express = require("express");
+// import
+const express = require("express");
 
-//import connection moule
-var conn= require("../config/db_connection");
+// import connection moule
+let conn = require("../config/db_connection");
 
-//getting the connection object
-var connection =conn.getConnection();
+// getting the connection object
+let connection = conn.getConnection();
 
-//connect to database
+// connect to database
 connection.connect();
 
-//create the module(Router)
-var router = express.Router();
-router.post("/",function(req,res){
+let router = express.Router();
+router.post("/", function(req, res) {
 //reading the parameters from client
-    var p_id=req.body.p_id;
-    var p_name=req.body.p_name;
-    var p_cost=req.body.p_cost;
+    var p_id = req.body.p_id;
+    var p_name = req.body.p_name;
+    var p_cost = req.body.p_cost;
 
     connection.query("insert into product values("+p_id+",'"+p_name+"', "+p_cost+")",
-        function (err,result){
-        if(err){
+        function (err, result) {
+        if (err) {
             console.log("error while inserting the object!")
-        }else{
-            res.send({insert:"success"});
+        } else {
+            res.send({ insert:"success" });
         }
     });
 });
-//export router
-module.exports=router;
+
+module.exports = router;
