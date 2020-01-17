99e7662b9257c553d5ecb9fb45af54cddaed036f
Pankaj Singh <singh.pankaj@live.com>
2020-01-17 13:46:28 +0530

    Update update.js

--- update/update.js
+++ update/update.js
@@ -5,21 +5,21 @@ const express = require("express");
 let conn = require("../config/db_connection");
 
 // getting the connection object
-var connection = conn.getConnection();
+let connection = conn.getConnection();
 
 // connect to database
 connection.connect();
 
 // create the module(Router)
 let router = express.Router();
-router.post("/",function(req, res) {
+router.post("/", function(req, res) {
 // reading the parameters from client
-    var p_id=req.body.p_id;
-    var p_name=req.body.p_name;
-    var p_cost=req.body.p_cost;
+    var p_id = req.body.p_id;
+    var p_name = req.body.p_name;
+    var p_cost = req.body.p_cost;
 
     connection.query("update product set p_name ='"+p_name +"',p_cost="+p_cost+" where p_id = "+ p_id,
-    function(err,result) {
+    function(err, result) {
         if (err) {
             console.log("error while updating the data !");
         } else {
