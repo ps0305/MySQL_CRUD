3dc1ab3e4de99bb765d7cc6e068973811763f074
Pankaj Singh <singh.pankaj@live.com>
2020-01-17 13:39:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-//importing modules
-var express= require("express");
-var fetch=require("./fetch/fetch");
-var bodyparser= require("body-parser");
+// importing modules(commonjs)
+const express = require("express");
+const fetch = require("./fetch/fetch");
+const bodyparser = require("body-parser");
 
-//create app instance
-var app=express();
-app.use("/fetch",fetch);
+// create app instance
+let app = express();
+app.use("/fetch", fetch);
 
-//set JSON as MIME type
+// set JSON as MIME type
 app.use(bodyparser.json());
 
-//front-end not sending any form data
-app.use(bodyparser.urlencoded({extended:false}));
+// check for form data
+app.use(bodyparser.urlencoded({ extended:false }));
 
 var insert = require("./insert/insert");
 app.use("/insert",insert);
 
-//update operation
-var update= require("./update/update");
-app.use("/update",update);
+// update
+let update = require("./update/update");
+app.use("/update", update);
 
-//delete opearation
-var remove=require("./delete/delete");
-app.use("/delete",remove);
+// delete
+let remove = require("./delete/delete");
+app.use("/delete", remove);
 
-//Assign port number
+// Assign port number
 app.listen(8080);
 console.log("server is listening at port no. 8080");
